3598c8831f771b8aa413019449850ed2e85138bb
HyenaWilhelm <matve9162160876@gmail.com>
2023-04-24 14:54:25 +0300

    Сделал 4 практическую

---
+++ Изменение/FirstPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Изменение.fovorite_colorDataSetTableAdapters;
+
+
+namespace Изменение
+{
+    /// <summary>
+    /// Логика взаимодействия для FirstPage.xaml
+    /// </summary>
+    public partial class FirstPage : Page
+    {
+        colorsTableAdapter adapter = new colorsTableAdapter();
+        public FirstPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonChange_Click(object sender, RoutedEventArgs e)
+        {
+            object id = (MyDataGrid.SelectedItem as DataRowView).Row[0];
+            adapter.UpdateQuery(MyTbx.Text, Convert.ToInt32(id));
+        }
+    }
+}
---
+++ Изменение/SecondPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Изменение.fovorite_colorDataSetTableAdapters;
+
+namespace Изменение
+{
+    /// <summary>
+    /// Логика взаимодействия для SecondPage.xaml
+    /// </summary>
+    public partial class SecondPage : Page
+    {
+        favourite_colorTableAdapter adapter = new favourite_colorTableAdapter();
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonChange_Click(object sender, RoutedEventArgs e)
+        {
+            object id = (MyDataGrid.SelectedItem as DataRowView).Row[0];
+            adapter.UpdateQuery(MyTbx.Text, Convert.ToInt32(id));
+        }
+    }
+}
